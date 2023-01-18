21a0665c0a32a490e8885f9b3ca9efdf0f067bb8
Jon Claxton <speedyclax@gmail.com>
2023-01-18 14:45:56 -0600

    completed attempt1 of exercise1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,8 +3,27 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {         
+
+            
+            var random = new Random();
+            var number = random.Next(1,10);
+
+            Console.WriteLine("Try to guess a number between 1 and 10.");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > number)
+            {
+                Console.WriteLine("Too high");
+                
+            }
+            else if (userInput < number)
+            {
+                Console.WriteLine("Too low");
+                
+            }
+            else { Console.WriteLine("That's correct!"); }
         }
     }
 }
